74b412f3513806b8a135a00480a351b677badc05
Tigran Grigoryan <tigran.grigoryan1995@gmail.com>
2018-09-24 22:28:22 +0400

    Local References

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -16,16 +16,18 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddServer() {
+  onAddServer(nameInput : HTMLInputElement) {
     this.serverCreated.emit({
-      serverName : this.newServerName,
+      // serverName : this.newServerName,
+      serverName : nameInput.value,
       serverContent : this.newServerContent
     })
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput : HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName : this.newServerName,
+      // serverName : this.newServerName,
+      serverName : nameInput.value,
       serverContent : this.newServerContent
     })
   }
