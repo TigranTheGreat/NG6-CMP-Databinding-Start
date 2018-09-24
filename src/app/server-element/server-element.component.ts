bb426a18bab6ae680c69b3cfb80e8124dd287da8
Tigran Grigoryan <tigran.grigoryan1995@gmail.com>
2018-09-24 21:31:28 +0400

    Input/Output (Prop/Event)

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
+  //encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ServerElementComponent implements OnInit {
+  @Input('data') element : { type: string, name: string, content: string };
 
   constructor() { }
 
